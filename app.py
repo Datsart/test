b81449118cbc4583ede8f09d6286e13c47c0d057
Artem <dacenko.aa9@gmail.com>
2024-10-02 19:26:02 +0300

    test API

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+import json
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 CORS(app)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+@app.route("/test", methods=["GET", "POST"])
+def func():
+    data = request.get_json()
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
---
+++ test.py
@@ -0,0 +1,10 @@
+import requests
+from random import randint
+
+req = requests.post(
+    "http://127.0.0.1:5000/test",
+    headers={"Content-type": "application/json"},
+    json=randint(1, 100),
+)
+
+print(req.text)
