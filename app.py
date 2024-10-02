a566386b234d98cb247bbdf2ea00baae9a284205
Artem <dacenko.aa9@gmail.com>
2024-10-02 18:42:59 +0300

    vs code

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__, template_folder='templates')
+CORS(app)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
