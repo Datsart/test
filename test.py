80513438d1e6357852a29e0c9193e5570d0e0fe1
Artem <dacenko.aa9@gmail.com>
2024-10-02 19:46:58 +0300

    test API

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import requests
 from random import randint
 
-req = requests.post(
-    "http://127.0.0.1:5000/test",
-    headers={"Content-type": "application/json"},
-    json=randint(1, 100),
-)
 
-print(req.text)
+def req():
+    req = requests.post(
+        "http://127.0.0.1:5000/test",
+        headers={"Content-type": "application/json"},
+        json=randint(1, 100),
+    )
+
+    print(req.text)
+
+
+print(123431)
