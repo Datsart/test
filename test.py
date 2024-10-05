0f48296de0192b15a2d2b5edd12b3d6f4317d701
artemdatsenko <dacenko.aa@yandex.ru>
2024-10-05 22:24:02 +0300

    fix

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import requests
 import random
 
-res = requests.post(url='http://127.0.0.1:8000/test', headers={'Content-type': 'application/json'},
-                    json=random.randint(1, 100))
-print(res.status_code)
+try:
+    res = requests.post(url='http://127.0.0.1:8000/test', headers={'Content-type': 'application/json'},
+                        json=random.randint(1, 100))
+    print(res.status_code)
+    print(res.text)
+except BaseException as e:
+    print('Ошибка нахуй -', e)
